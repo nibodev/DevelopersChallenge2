49b18414012813fe1e00a3d894369e9c5f446ff3
Denis Oliveira <denis.olvra@gmail.com>
2021-06-18 22:53:58 -0300

    First commit

---
+++ src/DevelopersChallenge2/DevelopersChallenge2.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevelopersChallenge2.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
