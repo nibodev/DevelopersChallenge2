0fc70e9d4f681b361972615533e1ebda70ca6539
Denis Oliveira <86087037+denis-olvra@users.noreply.github.com>
2021-06-20 02:13:04 -0300

    Update HomeController.cs

--- src/DevelopersChallenge2/DevelopersChallenge2.Application/Controllers/HomeController.cs
+++ src/DevelopersChallenge2/DevelopersChallenge2.Application/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using DevelopersChallenge2.Application.Models;
+﻿using DevelopersChallenge2.Application.Domain.Interfaces;
+using DevelopersChallenge2.Application.Models;
+using DevelopersChallenge2.Application.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace DevelopersChallenge2.Application.Controllers
@@ -12,10 +14,17 @@ namespace DevelopersChallenge2.Application.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IOfxService _ofxService;
+        private readonly IConfiguration _config;
+        private readonly ITransactionRepository _transactionRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IOfxService service, 
+            IConfiguration configuration, ITransactionRepository transactionRepository)
         {
             _logger = logger;
+            _ofxService = service;
+            _config = configuration;
+            _transactionRepository = transactionRepository;
         }
 
         public IActionResult Index()
@@ -23,7 +32,22 @@ namespace DevelopersChallenge2.Application.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> UploadFiles(List<IFormFile> FormFiles)
+        {
+            //TODO Validate files
+            await _ofxService.ProcessOfxFiles(FormFiles);
+
+            return RedirectToAction("Transactions", "Home");
+        }
+
+        public async Task<IActionResult> TransactionsAsync()
+        {
+            var transactions = await _transactionRepository.GetTransactionsWithoutDuplicates();
+            return View(transactions);
+        }
+
+        public IActionResult About()
         {
             return View();
         }
